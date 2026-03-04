5c28a6ad87479cb746b670cc590ad9246e02a72b
CodeSpace Academy <duwaynefrieslaar23@gmail.com>
2026-03-05 00:14:51 +0200

    change est date

--- grow-with-nadine/src/components/Footer.jsx
+++ grow-with-nadine/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-[#957F6A] text-white py-6 mt-8">
       <div className="max-w-6xl mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Grow By Nadine. All rights reserved.</p>
+        <p>&copy; {new Date("2024-01-01").getFullYear()} Grow By Nadine. All rights reserved.</p>
         <p>Call us: (+27) 731577339</p>
         <p>Email: info@growbynadine.com</p>
       </div>
